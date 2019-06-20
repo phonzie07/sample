fcac9501522b451ddcb5e63d97cdddf50c099a82
hans suarez <suarez.hans07@gmail.com>
2019-06-20 11:17:06 +0800

    edited eureka configs

--- src/main/java/com/sample/SampleApplication.java
+++ src/main/java/com/sample/SampleApplication.java
@@ -2,9 +2,10 @@ package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class SampleApplication {
 
 	public static void main(String[] args) {
