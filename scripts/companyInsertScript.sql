493feb015b9e4a0b6e4967285a6c46528b5a6684
hans suarez <suarez.hans07@gmail.com>
2019-06-20 14:12:45 +0800

    added insert script for company table

---
+++ scripts/companyInsertScript.sql
@@ -0,0 +1,12 @@
+﻿
+INSERT INTO public.company(
+             created_by, created_time, modified_by, modified_time, 
+            code, description, name)
+    VALUES ( 1, CURRENT_TIMESTAMP, null, null, 
+            'gs', 'gospace site', 'gospace'),
+            ( 1, CURRENT_TIMESTAMP, null, null, 
+            'fs', 'foose web', 'foose'),
+            ( 1, CURRENT_TIMESTAMP, null, null, 
+            'ws', 'workspace website', 'workspace'),
+            ( 1, CURRENT_TIMESTAMP, null, null, 
+            'cms', 'content management system', 'cms');
